e9bf7808df4847ecb77a33dd48f4ccffc5e5e48c
Daniel Lief <154765923+platinumblock@users.noreply.github.com>
2025-01-12 17:40:16 -0800

    Add files via upload

--- casestudy.js
+++ casestudy.js
@@ -1,24 +1,19 @@
 var body = document.body;
 var html = document.documentElement;
-// Height of page at 100% zoom (in pixels)
-var heightAt100 = 11596 // UPDATE THIS when you change the contents of the page
+var heightAt100 = localStorage.getItem("height100")
 var height = heightAt100; 
 var numerals = ["I", "II", "III", "IV", "V"]
 var ranges = []
 var contents = document.getElementsByClassName("content");
-// Wait for images to load
 setTimeout(() => {
-    // Set height of sidebar
     height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
     //console.log(height);
     document.getElementById("sidebar").style.height = (height - 400 * (height / heightAt100)) + "px";
-    // Make list of y cutoffs for sections
     for(let i = 0; i < contents.length; i++){
         var n = numerals[i];
         ranges.push(document.getElementById(n).offsetTop);
     }
 }, 100);
-// On scroll:
 function handleScroll(){
     var scroll = window.scrollY -  100 * (height / heightAt100);
     for(let i = 0; i < ranges.length; i++){
--- index.js
+++ index.js
@@ -1,33 +1,9 @@
-var body = document.body;
-var html = document.documentElement;
-// Get height of page (in pixels)
-var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-// Set height of sidebar
-document.getElementById("sidebar").style.height = height + "px";
-// Make list of y cutoffs for sections
-var numerals = ["I", "II", "III", "IV", "V"]
-var ranges = []
-var contents = document.getElementsByClassName("content");
-for(let i = 0; i < contents.length; i++){
-    var n = numerals[i]
-    ranges.push(document.getElementById(n).offsetTop + document.getElementById(n).style.height)
-}
-// Height of page at 100% zoom (in pixels)
-var heightAt100 = 5400
-// On scroll:
-function handleScroll(){
-    var scroll = window.scrollY -  200 * (height / heightAt100);
-    for(let i = 0; i < ranges.length; i++){
-        if(scroll > ranges[i] && (i == ranges.length - 1 || scroll < ranges[i + 1])){
-            for(let j = 0; j < contents.length; j++){
-                if(i == j){
-                    contents[j].style.color = "var(--darkprimary)";
-                }else{
-                    contents[j].style.color = "var(--darksecondary)";
-                }
-            }
-            break;
-        }
+function view(project){
+    if(project == "doorlist"){
+        localStorage.setItem("height100", 11596);
     }
+    if(project == "converse"){
+        localStorage.setItem("height100", 3115);
+    }
+    window.location.href = project + ".html"
 }
-window.addEventListener("scroll", handleScroll)
